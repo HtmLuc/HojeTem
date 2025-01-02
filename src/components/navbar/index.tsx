1fe019196533a5d09ec2bd9a0c66206ebc5634e1
Lucas Dantas <lucasvdmedeiros@proton.me>
2025-01-01 21:57:06 -0300

    first commit

---
+++ src/components/navbar/index.tsx
@@ -0,0 +1,7 @@
+const Navbar = () => {
+  return (
+    <div className="bg-blue-600">Navbar</div>
+  )
+}
+
+export default Navbar
---
+++ src/pages/sobreEvento.tsx
@@ -0,0 +1,9 @@
+const SobreEvento = () => {
+  return (
+    <>
+      <h1>Página com informes dos eventos</h1>
+    </>
+  )
+}
+
+export default SobreEvento
