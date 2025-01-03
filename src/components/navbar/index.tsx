2a986272a8e4caf5e9327ad5de32f147ee81241c
Lucas Dantas <lucasvdmedeiros@proton.me>
2025-01-02 23:35:29 -0300

    feat: instalação de novas dependências

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,6 +1,31 @@
+import { Disclosure } from '@headlessui/react'
+
 const Navbar = () => {
+  const user = {
+    name: 'Lucas Dantas',
+    email: 'lucas@proton.me',
+    imageUrl:
+    'https://avatars.githubusercontent.com/u/51703909',
+  }
+  const userNavigation = [
+    { name: 'Perfil', href: '/user/perfil' },
+    { name: 'Seus eventos', href: 'user/eventos' },
+    { name: 'Sair', href: '/logout' },
+  ]
   return (
-    <div className="bg-blue-600">Navbar</div>
+    <div className="min-h-full">
+      <Disclosure as="nav" className="bg-secondary">
+        <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+          <div className="flex h-16 items-center justify-between">
+            <div className="flex items-center">
+              <div className="shrink-0">
+                <h1 className="font-palette-mosaic">Hoje Tem</h1>
+              </div>
+            </div>
+          </div>
+        </div>
+      </Disclosure>
+    </div>
   )
 }
 
