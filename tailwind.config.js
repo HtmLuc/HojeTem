72baccab9a7e536bbb1c5ea6df41b9343004d8ef
Lucas Dantas <lucasvdmedeiros@proton.me>
2025-01-02 23:34:06 -0300

    fix: atualizando fontes e cores padrão

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,16 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    colors: {
+      'primary': '#F5E6CC',
+      'secondary': '#D16B48',
+    },
+    fontFamily: {
+        'palette-mosaic': ['Palette Mosaic'],
+        'antic-didone': ['Antic Didone'],
+        'alegreya Sans': ['Alegreya Sans'],
+        'poppins': ['Poppins'],
+    },
     extend: {},
   },
   plugins: [],
