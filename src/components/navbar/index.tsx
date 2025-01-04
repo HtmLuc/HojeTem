d9cef4adc43d0ab769d72fbdd9d05e554040db3d
Lucas Dantas <lucasvdmedeiros@proton.me>
2025-01-04 15:01:56 -0300

    feat: criação da navbar

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,4 +1,5 @@
-import { Disclosure } from '@headlessui/react'
+import { Disclosure, DisclosureButton, DisclosurePanel, Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react'
+import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
 
 const Navbar = () => {
   const user = {
@@ -12,21 +13,72 @@ const Navbar = () => {
     { name: 'Seus eventos', href: 'user/eventos' },
     { name: 'Sair', href: '/logout' },
   ]
+  function classNames(...classes) {
+    return classes.filter(Boolean).join(' ')
+  }
   return (
-    <div className="min-h-full">
-      <Disclosure as="nav" className="bg-secondary">
+    <div className="bg-secondary">
+      <Disclosure as="nav">
         <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
           <div className="flex h-16 items-center justify-between">
             <div className="flex items-center">
               <div className="shrink-0">
-                <h1 className="font-palette-mosaic">Hoje Tem</h1>
+                <a href="/" className="font-modak text-5xl">Hoje Tem</a>
               </div>
+            </div>
+            <div className="hidden md:block">
+              <div className="ml-4 flex items-center md:ml-6">
+                {/* Botão de notificacões (NÃO SEI SE ELE VAI FICAR AQUI) */}
+                <button
+                  type="button"
+                  className="relative rounded-full bg-gray-800 p-1 text-gray-300 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+                >
+                  <span className="absolute -inset-1.5" />
+                  <span className="sr-only">Mostrar notificações</span>
+                  <BellIcon aria-hidden="true" className="size-6" />
+                </button>
+
+                {/* Dropdown do perfil */}
+                <Menu as="div" className="relative ml-3">
+                  <div>
+                    <MenuButton className="relative flex max-w-xs items-center rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+                      <span className="absolute -inset-1.5" />
+                      <span className="sr-only">Menu do usuário</span>
+                      <img alt="" src={user.imageUrl} className="size-8 rounded-full" />
+                    </MenuButton>
+                  </div>
+                  <MenuItems
+                    transition
+                    className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md py-1 shadow-lg bg-white ring-1 ring-black/50 transition focus:outline-none data-[closed]:scale-95 data-[closed]:transform data-[closed]:opacity-0 data-[enter]:duration-100 data-[leave]:duration-75 data-[enter]:ease-out data-[leave]:ease-in"
+                  >
+                    {userNavigation.map((item) => (
+                      <MenuItem key={item.name}>
+                        <a
+                          href={item.href}
+                          className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:outline-none"
+                        >
+                          {item.name}
+                        </a>
+                      </MenuItem>
+                    ))}
+                  </MenuItems>
+                </Menu>
+              </div>
+            </div>
+            <div className="-mr-2 flex md:hidden">
+              {/* Menu papra telas menores */}
+              <DisclosureButton className="group relative inline-flex items-center justify-center rounded-md bg-gray-800 p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+                <span className="absolute -inset-0.5" />
+                <span className="sr-only">Abrir menu principal</span>
+                <Bars3Icon aria-hidden="true" className="block size-6 group-data-[open]:hidden" />
+                <XMarkIcon aria-hidden="true" className="hidden size-6 group-data-[open]:block" />
+              </DisclosureButton>
             </div>
           </div>
         </div>
       </Disclosure>
     </div>
-  )
+  );
 }
 
 export default Navbar
