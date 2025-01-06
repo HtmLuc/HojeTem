ae15ce26f20cf492747c11c0a457ab063f9587cf
Lucas Dantas <lucasvdmedeiros@proton.me>
2025-01-05 22:15:25 -0300

    fix: correção na transição na rolagem dos links e icone papssanod por cima da navbar

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -15,22 +15,22 @@ const Home = () => {
   ]
   return (
     <>
-      <header>
+      <header className="relative z-50">
         <Navbar />
       </header>
       <main>
-        <section className="bg-tertiary h-screen text-white">
-          <div className="flex justify-between pt-16">
+        <section className="bg-tertiary h-screen text-white pt-16">
+          <div className="flex justify-between">
             <div className="flex flex-col justify-center text-center w-5/12 ml-20">
               <h2 className="font-antic-didone text-4xl mb-14">Procurando eventos em Natal e não sabe por onde começar?</h2>
               <div>
                 <h3 className="font-alegreya-sans font-black text-xl pb-5">Explore para encontrar</h3>
                 <div className="flex justify-between">
                   {navegacao.map(elemento => (
-                    <div className="flex flex-col max-w-28">
+                    <a className="flex flex-col max-w-28" href={elemento.href}>
                       <img alt={elemento.descricaco} src={elemento.imageUrl} className="w-28 h-28 rounded-md" />
                       <p className="font-antic-didone leading-5 text-md pt-1">{elemento.descricao}</p>
-                    </div>
+                    </a>
                   ))}
                 </div>
                 <h3 className="font-alegreya-sans font-black text-xl pt-5">e mais ...</h3>
